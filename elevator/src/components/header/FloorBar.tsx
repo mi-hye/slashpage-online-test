d1b6aba4e2c38ada7200ee658daec295fce1eef4
mi-hye <satge13@gmail.com>
2024-06-24 22:03:43 +0900

    feat: Add state isAllActive

--- elevator/src/components/header/FloorBar.tsx
+++ elevator/src/components/header/FloorBar.tsx
@@ -1,16 +1,30 @@
+import { useContext } from "react";
 import FloorButton from "./FloorButton";
+import { ElevatorContext } from "../../provider/ElevatorManagerProvider";
 
 const TOTAL_FLOOR = 15;
 
 function Floor() {
+	const { isAllActive, setTargetFloor } =
+		useContext(ElevatorContext);
+
 	return (
 		<header className="my-5 w-[90%] h-13 flex">
 			<span className="w-10 mx-5 flex-center font-semibold">
 				호출
 			</span>
-			<div className="w-full h-full flex justify-evenly border border-r-0">
+			<div
+				className={`w-full h-full flex justify-evenly border border-r-0 ${
+					isAllActive ? "bg-gray-200 text-black" : ""
+				}`}
+			>
 				{Array.from({ length: TOTAL_FLOOR }, (_, i) => (
-					<FloorButton key={i + 1} floor={i + 1} />
+					<FloorButton
+						key={i + 1}
+						floor={i + 1}
+						setTargetFloor={setTargetFloor}
+						isAllActive={isAllActive}
+					/>
 				))}
 			</div>
 		</header>
--- elevator/src/components/header/FloorButton.tsx
+++ elevator/src/components/header/FloorButton.tsx
@@ -2,19 +2,31 @@ import { useState } from "react";
 
 interface PropsType {
 	floor: number;
+	setTargetFloor: React.Dispatch<
+		React.SetStateAction<number>
+	>;
+	isAllActive: boolean;
 }
 
 const activeStyle = "font-bold text-red-500";
 
-function FloorButton({ floor }: PropsType) {
+function FloorButton({
+	floor,
+	setTargetFloor,
+	isAllActive,
+}: PropsType) {
 	const [active, setActive] = useState(false);
 
-	const handleActive = () => setActive(true);
+	const handleActive = () => {
+		setActive(true);
+		setTargetFloor(floor);
+	};
+
 	return (
 		<div
 			className={`cursor-pointer h-full flex-center border-r w-full ${
 				active ? activeStyle : ""
-			}`}
+			} ${isAllActive ? "text-black" : ""}`}
 			onClick={handleActive}
 		>
 			{floor}
