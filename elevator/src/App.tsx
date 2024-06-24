e006ae4ab5b9de07986fe80582f1e58663969497
mi-hye <satge13@gmail.com>
2024-06-24 21:04:35 +0900

    feat: Add ElevatorManager provider

--- elevator/src/App.tsx
+++ elevator/src/App.tsx
@@ -1,11 +1,14 @@
 import Main from "./components/Main/Main";
 import Floor from "./components/header/FloorBar";
+import { ElevatorManagerProvider } from "./provider/ElevatorManagerProvider";
 
 function App() {
 	return (
 		<div className="w-screen h-screen flex flex-col justify-center min-w-[768px] min-h-[700px]">
-			<Floor />
-			<Main />
+			<ElevatorManagerProvider>
+				<Floor />
+				<Main />
+			</ElevatorManagerProvider>
 		</div>
 	);
 }
